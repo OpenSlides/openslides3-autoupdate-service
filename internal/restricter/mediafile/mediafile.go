04d0ecd496e63b5071f5ca680662298cc4788304
Oskar Hahn <mail@oshahn.de>
2020-06-10 10:58:11 +0200

    Clean up restricter code by using ElementFunc (#13)

--- internal/restricter/mediafile/mediafile.go
+++ internal/restricter/mediafile/mediafile.go
@@ -3,47 +3,47 @@ package mediafile
 import (
 	"encoding/json"
 	"fmt"
+
+	"github.com/OpenSlides/openslides3-autoupdate-service/internal/restricter"
 )
 
-// Mediafile restricts a mediafile object.
-type Mediafile struct {
-	required
+type required interface {
+	restricter.HasPermer
+	IsSuperadmin(uid int) bool
+	InGroups(uid int, groups []int) bool
 }
 
-// New creates a new Mediafile instance.
-func New(r required) *Mediafile {
-	return &Mediafile{r}
-}
+// Restrict restricts a mediafile object.
+func Restrict(r required) restricter.ElementFunc {
+	return func(uid int, data json.RawMessage) (json.RawMessage, error) {
+		if !r.HasPerm(uid, "mediafiles.can_see") {
+			return nil, nil
+		}
 
-// Restrict restricts one mediafile.
-func (m *Mediafile) Restrict(uid int, data json.RawMessage) (json.RawMessage, error) {
-	if !m.HasPerm(uid, "mediafiles.can_see") {
+		if r.IsSuperadmin(uid) {
+			return data, nil
+		}
+
+		var media struct {
+			InheritedAccess interface{} `json:"inherited_access_groups_id"`
+		}
+
+		if err := json.Unmarshal(data, &media); err != nil {
+			return nil, fmt.Errorf("decoding mediafile: %w", err)
+		}
+
+		switch accessGroups := media.InheritedAccess.(type) {
+		case bool:
+			if accessGroups {
+				return data, nil
+			}
+		case []int:
+			if r.InGroups(uid, accessGroups) {
+				return data, nil
+			}
+		default:
+			return nil, fmt.Errorf("invalid type %T for mediafile.inherited_access_groups_id", accessGroups)
+		}
 		return nil, nil
 	}
-
-	if m.IsSuperadmin(uid) {
-		return data, nil
-	}
-
-	var media struct {
-		InheritedAccess interface{} `json:"inherited_access_groups_id"`
-	}
-
-	if err := json.Unmarshal(data, &media); err != nil {
-		return nil, fmt.Errorf("decoding mediafile: %w", err)
-	}
-
-	switch accessGroups := media.InheritedAccess.(type) {
-	case bool:
-		if accessGroups {
-			return data, nil
-		}
-	case []int:
-		if m.InGroups(uid, accessGroups) {
-			return data, nil
-		}
-	default:
-		return nil, fmt.Errorf("invalid type %T for mediafile.inherited_access_groups_id", accessGroups)
-	}
-	return nil, nil
 }
--- internal/restricter/motion/interface.go
+++
@@ -1,8 +0,0 @@
-package motion
-
-import "github.com/OpenSlides/openslides3-autoupdate-service/internal/restricter"
-
-type required interface {
-	restricter.HasPermer
-	InGroups(uid int, groups []int) bool
-}
