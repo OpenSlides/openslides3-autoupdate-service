ee9fa8fd47dec1878ce1d258afebfc4924ed47aa
Oskar Hahn <mail@oshahn.de>
2020-06-10 13:10:55 +0200

    user restrictrer call require user

--- TODO.md
+++ TODO.md
@@ -1,6 +1,7 @@
 * Projector
 * Notify
 * Startup with incomplete redis data
+* Restricter: Check permission of required user
 
 
 * Delete TODO.md and KONZEPT.md
--- internal/restricter/user/user.go
+++ internal/restricter/user/user.go
@@ -3,13 +3,17 @@ package user
 import (
 	"encoding/json"
 	"fmt"
-	"strconv"
 
 	"github.com/OpenSlides/openslides3-autoupdate-service/internal/restricter"
 )
 
+type required interface {
+	restricter.HasPermer
+	UserRequired(uid int) bool
+}
+
 // Restrict handels restrictions of users/user elements.
-func Restrict(r restricter.HasPermer) restricter.ElementFunc {
+func Restrict(r required) restricter.ElementFunc {
 	return func(uid int, element json.RawMessage) (json.RawMessage, error) {
 		littleDataFields := []string{
 			"id",
@@ -30,17 +34,32 @@ func Restrict(r restricter.HasPermer) restricter.ElementFunc {
 		allDataFields := append(manyDataFields, "default_password")
 		ownDataFields := append(littleDataFields, "email", "gender")
 
+		var user struct {
+			ID int `json:"id"`
+		}
+		if err := json.Unmarshal(element, &user); err != nil {
+			return nil, fmt.Errorf("unmarshal user: %w", err)
+		}
+		if user.ID == uid && !r.HasPerm(uid, "users.can_see_extra_data") {
+			return filter(element, ownDataFields)
+		}
+
 		if r.HasPerm(uid, "users.can_see_name") {
 			if r.HasPerm(uid, "users.can_see_extra_data") {
 				if r.HasPerm(uid, "users.can_manage") {
-					return filter(element, allDataFields, 0, nil)
+					return filter(element, allDataFields)
 				}
-				return filter(element, manyDataFields, 0, nil)
+				return filter(element, manyDataFields)
 			}
-			return filter(element, littleDataFields, uid, ownDataFields)
+			return filter(element, littleDataFields)
 		}
-		// TODO: build list of needed users
-		return nil, nil
+
+		if !r.UserRequired(user.ID) {
+			return nil, nil
+		}
+
+		// TODO: check permission of required user
+		return filter(element, littleDataFields)
 	}
 }
 
@@ -64,7 +83,7 @@ func PersonalNoteRestrict(uid int, data json.RawMessage) (json.RawMessage, error
 	return data, nil
 }
 
-func filter(value json.RawMessage, fields []string, uid int, ownfields []string) (json.RawMessage, error) {
+func filter(value json.RawMessage, fields []string) (json.RawMessage, error) {
 	var allData map[string]json.RawMessage
 	if err := json.Unmarshal(value, &allData); err != nil {
 		return nil, fmt.Errorf("unmarshall data: %w", err)
@@ -72,17 +91,7 @@ func filter(value json.RawMessage, fields []string, uid int, ownfields []string)
 
 	filteredData := make(map[string]json.RawMessage, len(fields))
 
-	id, err := strconv.Atoi(string(allData["id"]))
-	if err != nil {
-		return nil, fmt.Errorf("invalid id: %s", allData["id"])
-	}
-
-	useFields := fields
-	if ownfields != nil && uid == id {
-		useFields = ownfields
-	}
-
-	for _, k := range useFields {
+	for _, k := range fields {
 		filteredData[k] = allData[k]
 	}
 
