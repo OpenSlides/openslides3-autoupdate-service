bacf837357d6aacbbbb352db1f0ffbb4c40c5c73
Oskar Hahn <mail@oshahn.de>
2021-03-19 09:30:52 +0100

    Remove log output

--- internal/apps/assignment/required_users.go
+++ internal/apps/assignment/required_users.go
@@ -8,7 +8,6 @@ import (
 
 // RequiredAssignments returns the user ids of a assignment element.
 func RequiredAssignments(data json.RawMessage) (map[int]bool, string, error) {
-	log.Println("HierXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
 	var assignment struct {
 		Users []struct {
 			ID int `json:"user_id"`
