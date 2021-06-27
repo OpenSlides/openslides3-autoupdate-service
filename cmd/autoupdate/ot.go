0c7fc3c7caf70b974e721f072ba569ce4d762593
Oskar Hahn <mail@oshahn.de>
2021-06-27 09:01:48 +0200

    Update dependencies (#180)

--- cmd/autoupdate/ot.go
+++ cmd/autoupdate/ot.go
@@ -6,11 +6,11 @@ import (
 	"time"
 
 	"go.opentelemetry.io/contrib/instrumentation/runtime"
-	"go.opentelemetry.io/otel/exporters/metric/prometheus"
+	"go.opentelemetry.io/otel/exporters/prometheus"
 )
 
 func initMeter(mux *http.ServeMux) error {
-	exporter, err := prometheus.InstallNewPipeline(prometheus.Config{})
+	exporter, err := prometheus.New(prometheus.Config{}, nil)
 	if err != nil {
 		return fmt.Errorf("initialize prometheus pipeline: %w", err)
 	}
